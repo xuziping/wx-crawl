e4b90a0a0e9fb04a6b54e9711497bee0d61fecd3
za-xuzhiping <xuzp2005@21cn.com>
2018-08-08 16:43:25 +0800

    proxy crawler

--- src/main/java/com/xuzp/DemoRandomProxyCrawler.java
+++ src/main/java/com/xuzp/DemoRandomProxyCrawler.java
@@ -3,8 +3,8 @@ package com.xuzp;
 import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
 import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.net.Proxys;
-import cn.edu.hfut.dmic.webcollector.plugin.net.OkHttpRequester;
 import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
+import cn.edu.hfut.dmic.webcollector.plugin.net.OkHttpRequester;
 import okhttp3.OkHttpClient;
 
 import java.io.IOException;
@@ -17,26 +17,18 @@ import java.util.List;
 
 /**
  * 教程：WebCollector随机代理
- *
- * @author hu
  */
 public class DemoRandomProxyCrawler extends BreadthCrawler {
 
-    // 自定义的请求插件
-    // 可以设置随机代理选择器
     public static class MyRequester extends OkHttpRequester {
 
-        Proxys proxies;
-
-        public MyRequester() {
-            proxies = new Proxys();
-            // add a socks proxy
-            proxies.add("116.62.194.248", 3128);
-            proxies.add("113.200.56.13", 8010);
-            proxies.add("218.60.8.99", 3129);
-//            proxies.add("58.253.108.214", 80);
-//            proxies.add("220.249.185.178", 9797);
-        }
+//        static Proxys proxies = new Proxys();
+//
+//        static {
+//            proxies.add("116.62.194.248", 3128);
+//            proxies.add("113.200.56.13", 8010);
+//            proxies.add("218.60.8.99", 3129);
+//        }
 
         @Override
         public OkHttpClient.Builder createOkHttpClientBuilder() {
@@ -44,10 +36,16 @@ public class DemoRandomProxyCrawler extends BreadthCrawler {
                     .proxySelector(new ProxySelector() {
                         @Override
                         public List<Proxy> select(URI uri) {
+                            Proxys proxies = new Proxys();
+                            proxies.add("116.62.194.248", 3128);
+                            proxies.add("113.200.56.13", 8010);
+                            proxies.add("218.60.8.99", 3129);
                             Proxy randomProxy = proxies.nextRandom();
                             List<Proxy> randomProxies = new ArrayList<Proxy>();
                             if(randomProxy != null) {
                                 randomProxies.add(randomProxy);
+                            } else {
+                                randomProxies.add(Proxy.NO_PROXY);
                             }
                             System.out.println("Random Proxies:" + randomProxies);
                             return randomProxies;
@@ -63,20 +61,27 @@ public class DemoRandomProxyCrawler extends BreadthCrawler {
 
     public DemoRandomProxyCrawler(String crawlPath) {
         super(crawlPath, true);
-        setRequester(new MyRequester());
-
-        addSeed("https://www.baidu.com/");
-        addRegex("https://www.baidu.com/.*");
-
+        addSeed("http://pv.sohu.com/cityjson");
+        addSeed("http://pv.sohu.com/cityjson?ie=utf-8");
+//        addRegex("http://pv.sohu.com/cityjson/.*");
     }
 
     @Override
     public void visit(Page page, CrawlDatums crawlDatums) {
-        System.out.println(page.doc().title());
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LOG.info(page.html());
     }
 
     public static void main(String[] args) throws Exception {
-        DemoRandomProxyCrawler crawler = new DemoRandomProxyCrawler("crawl");
-        crawler.start(5);
+        DemoRandomProxyCrawler crawler = new DemoRandomProxyCrawler("proxy_crawl");
+        for(int i=0; i<10; i++) {
+            LOG.info("#############################################");
+            crawler.setRequester(new MyRequester());
+            crawler.start(20);
+        }
     }
 }
